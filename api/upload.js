f340ff5eeba3d2e0f35f9a3e29a7990c9bec7208
sittallchat <sittallchat@protonmail.com>
2025-05-15 20:50:40 +0300

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,3 +1,17 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Her yerden istek kabul eder
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (req.method !== 'POST') {
+    return res.status(405).send("Sadece POST istekleri destekleniyor.");
+  }
+}
+
 const formidable = require('formidable');
 const fs = require('fs');
 const path = require('path');
